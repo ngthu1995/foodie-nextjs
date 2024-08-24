8833e36a0845d9a8331f317e96410b92ae2f0bf3
ngthu1995 <ngthu1995@gmail.com>
2024-08-23 23:21:19 -0400

    add default pages for the web, allow user to save a meal

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,12 @@ import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default async function MealsPage() {
   const meals = await getMeals();
@@ -22,7 +28,11 @@ export default async function MealsPage() {
         </div>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
